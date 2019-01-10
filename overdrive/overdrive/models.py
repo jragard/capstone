e908bb57f7bba0a735bf127c942236ed974f645d
Ryan <jragard@gmail.com>
2019-01-10 13:54:43 -0500

    added due date to 'thanks.html' when user checks out a book.  Added due_date and no_of_licenses to Book model to represent book access expiration and the number of copies of each book available, respectively.  Fixed bug in views that was preventing the hold lists on books from updating.

--- overdrive/overdrive/models.py
+++ overdrive/overdrive/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 class Book(models.Model):
     title = models.CharField(max_length=100)
     checked_out_count = models.IntegerField(default=0)
-    hold_list = models.ManyToManyField(User)
+    hold_list = models.ManyToManyField(User, default=None)
+    no_of_licenses = models.IntegerField(default=0)
+    due_date = models.DateField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
         return self.title
@@ -14,8 +16,7 @@ class Book(models.Model):
 class OverdriveUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=12)
-    books_checked_out = models.ManyToManyField(Book)
+    books_checked_out = models.ManyToManyField(Book, default=None)
 
     def __str__(self):
         return self.username
-
--- overdrive/overdrive/views.py
+++ overdrive/overdrive/views.py
@@ -4,23 +4,23 @@ from django.contrib.auth.models import User
 from django.shortcuts import reverse, render, HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
 
+import datetime
+
 
 def home_view(request):
+
     books = Book.objects.all()
     books_lst = []
 
     if request.user.is_authenticated:
         current_user = OverdriveUser.objects.get(id=request.user.id)
-        user_books_list = [
-            book.title for book in current_user.books_checked_out.all()]
-        print(user_books_list)
+        user_books_list = [book.title for book in
+                           current_user.books_checked_out.all()]
 
         for book in books:
             if book.checked_out_count < 0:
                 book.checked_out_count = 0
                 book.save()
-            print(book.hold_list.all())
-            print(book.checked_out_count)
 
         for x in books:
             books_lst.append(x.title.replace(' ', '_'))
@@ -36,7 +36,7 @@ def home_view(request):
 
 
 def mybooks_view(request):
-    print(request.user)
+
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     current_user = OverdriveUser.objects.get(id=request.user.id)
@@ -50,66 +50,66 @@ def mybooks_view(request):
 
 
 def thanks_view(request):
+
     title = request.POST.get('title').replace(' ', '_')
-    current_user = OverdriveUser.objects.get(id=request.user.id)
+    current_user = OverdriveUser.objects.filter(id=request.user.id).first()
     book = Book.objects.get(title=title)
 
     current_user.books_checked_out.add(book)
-    current_user.save()
-
     book.checked_out_count += 1
+
+    current_user.save()
     book.save()
 
-    checked_out_count = book.checked_out_count
-    print(checked_out_count)
+    today = datetime.datetime.now().date()
 
-    print(current_user.books_checked_out.all())
+    due = today + datetime.timedelta(14)
 
-    return render(request, 'thanks.html')
+    book.due_date = due
+    book.save()
+
+    return render(request, 'thanks.html', {'due_date': due})
 
 
 def return_view(request, url):
-    print(url)
-    current_user = OverdriveUser.objects.get(id=request.user.id)
-    book_to_return = Book.objects.get(title=url)
 
-    hold_list = [user for user in book_to_return.hold_list.all()]
-    print(len(hold_list))
+    current_user = request.user.overdriveuser
+    book_to_return = Book.objects.get(title=url)
+    hold_list = book_to_return.hold_list.all()
 
     current_user.books_checked_out.remove(book_to_return)
-    current_user.save()
-
     book_to_return.checked_out_count -= 1
+
+    current_user.save()
     book_to_return.save()
 
-    if len(hold_list) is not 0:
-        print('-----------')
-        print(hold_list[0])
-        print('------------')
-        user_next_in_line = OverdriveUser.objects.get(user=hold_list[0])
+    if hold_list.count() > 0:
+
+        user_next_in_line = OverdriveUser.objects.get(user=hold_list.first())
+
         user_next_in_line.books_checked_out.add(book_to_return)
-        user_next_in_line.save()
-
-        book_to_return.hold_list.remove(current_user.user)
-        book_to_return.save()
-
+        book_to_return.hold_list.remove(user_next_in_line.user)
         book_to_return.checked_out_count += 1
+
+        user_next_in_line.save()
         book_to_return.save()
 
     return render(request, 'return_thanks.html')
 
 
 def hold_view(request, url):
+
     current_user = OverdriveUser.objects.get(id=request.user.id)
     book_to_hold = Book.objects.get(title=url)
 
     book_to_hold.hold_list.add(current_user.user)
     book_to_hold.save()
-    print(book_to_hold.hold_list.all())
+
     return render(request, 'hold_thanks.html')
 
 
 def content_view(request, url):
+
     html = 'checkout.html'
     content_html = 'content/' + url + '.html'
     unavailable_html = 'sorry.html'
@@ -120,13 +120,11 @@ def content_view(request, url):
     current_user = OverdriveUser.objects.get(id=request.user.id)
     book = Book.objects.get(title=url)
     books_list = [bk for bk in current_user.books_checked_out.all()]
-    # print(books_list)
 
     for bk in books_list:
-        print(book.checked_out_count)
         if url == bk.title:
             return render(request, content_html)
-    if url not in books_list and book.checked_out_count == 3:
+    if url not in books_list and book.checked_out_count == book.no_of_licenses:
         return render(request, unavailable_html, {'title': url.replace('_', ' '),
                                                   'url': url})
     else:
@@ -162,6 +160,7 @@ def signup_view(request):
 
 
 def login_view(request):
+
     if request.user.is_authenticated:
         return HttpResponseRedirect(reverse('homepage'))
 
@@ -182,5 +181,6 @@ def login_view(request):
 
 
 def logout_view(request):
+
     logout(request)
     return HttpResponseRedirect(reverse('homepage'))
